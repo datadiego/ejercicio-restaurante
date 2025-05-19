48feacaa708460027cbc4829f3ae006fd10a47b4
datadiego <juandiegomariscal@gmail.com>
2025-05-19 11:54:26 +0200

    update

--- build.js
+++ build.js
@@ -1,27 +1,27 @@
-import {menu} from './menu.js';
-import nunjucks from 'nunjucks';
-import fs from 'fs';
+import { menu } from './menu.js'
+import nunjucks from 'nunjucks'
+import fs from 'fs'
 
-function generateHTML(template, data) {
-    const templateContent = fs.readFileSync(template, 'utf-8');
-    const html = nunjucks.renderString(templateContent, data);
-    return html;
+function generateHTML (template, data) {
+  const templateContent = fs.readFileSync(template, 'utf-8')
+  const html = nunjucks.renderString(templateContent, data)
+  return html
 }
-function createHTMLFile(template, data, outputFile) {
-    const html = generateHTML(template, data);
-    fs.writeFileSync(outputFile, html, 'utf-8');
-    console.log(`Archivo ${outputFile} generado con éxito.`);
+function createHTMLFile (template, data, outputFile) {
+  const html = generateHTML(template, data)
+  fs.writeFileSync(outputFile, html, 'utf-8')
+  console.log(`Archivo ${outputFile} generado con éxito.`)
 }
 
 if (!fs.existsSync('./dist')) {
-    fs.mkdirSync('./dist');
+  fs.mkdirSync('./dist')
 }
-const menuTemplate = './views/menu.njk';
-nunjucks.configure("views",{ autoescape: true });
+const menuTemplate = './views/menu.njk'
+nunjucks.configure('views', { autoescape: true })
 
-createHTMLFile(menuTemplate, { menu }, './dist/index.html');
-
-
-const cssFile = 'style.css';
-const outputCssFile = './dist/style.css';
-fs.copyFileSync(cssFile, outputCssFile);
+createHTMLFile(menuTemplate, { menu }, './dist/index.html')
+/*
+const cssFile = 'style.css'
+const outputCssFile = './dist/style.css'
+fs.copyFileSync(cssFile, outputCssFile)
+*/
